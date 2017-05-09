fb24bd065844117e9bfb4267e261e590956d89ba
“Anais <ananavelez@gmail.com>
2017-05-08 22:37:43 -0300

    primer commit anisen

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
+
 var x = prompt("datos para imc:");
- x.innerHTML
+ 
   alert("aleta de mensaje");
+
+  
+  function calcularPeso(){//desde aqui es el codigo de anisen
+  	var altura=document.getElementById("valoraltura").value;
+  	var peso=document.getElementById("valorpeso").value;
+  	var guardar=document.getElementById("guardar");
+  	var resultado=peso/Math.pow(altura,2);
+    alert(resultado);
+  };//hasta aqui :)
+  
